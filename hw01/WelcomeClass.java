69c0849e5c841e5866192f87f7f0ed9dcf560f20
sonnydiao <shd314@lehigh.edu>
2015-01-23 15:29:08 +0000

    updated HelloWorld.java

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,12 @@
+// hw01 Welcome Class
+// Shuotao Diao (shd314@lehigh.edu)
+public class WelcomeClass {
+    public static void main(String [] args){
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("^  ^  ^  ^  ^  ^");
+        System.out.println("/ \/ \/ \/ \/ \/ \");
+        System.out.println("");
+    }
+}
---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,15 @@
+// HelloWorld 
+// Sonnydiao 1st program 
+// This program aims to display "HelloWorld" on the screen 
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("######################");
+        System.out.println("######################");
+        System.out.println("#                    #");
+        System.out.println("# Hello, World 2015  #");
+        System.out.println("######################");
+        System.out.println("######################");
+        System.out.println("test");
+    }
+}
+    
