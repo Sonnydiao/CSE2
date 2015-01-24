59306b0df74f457dd2042aeb957aeab62bbadc6b
sonnydiao <shd314@lehigh.edu>
2015-01-24 19:38:42 +0000

    updated WelcomeClass

--- hw01/WelcomeClass.java
+++ hw01/WelcomeClass.java
@@ -1,12 +1,17 @@
 // hw01 Welcome Class
-// Shuotao Diao (shd314@lehigh.edu)
+// Shuotao Diao (shd314@lehigh.edu) CSE2
 public class WelcomeClass {
     public static void main(String [] args){
-        System.out.println("-----------");
-        System.out.println("| WELCOME |");
-        System.out.println("-----------");
-        System.out.println("^  ^  ^  ^  ^  ^");
-        System.out.println("/ \/ \/ \/ \/ \/ \");
-        System.out.println("");
+        // display 
+        System.out.println("-------------");
+        System.out.println("|  WELCOME  | ");
+        System.out.println("-------------");
+        System.out.println(" ^  ^  ^  ^  ^  ^ ");
+        System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<S--H--D--3--1--4>");
+        System.out.println("\\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println(" v  v  v  v  v  v  ");
+        System.out.println("Life is like a box of chocolate,"+
+        "you never know what you are going to get.");
     }
-}
+}    
