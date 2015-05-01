1cd075dbf6f97294eb38852d2d44b064d8a797a1
sonnydiao <shd314@lehigh.edu>
2015-05-01 15:03:25 +0000

    updatde FourDwins

--- hw13/FourDwins.java
+++ hw13/FourDwins.java
@@ -43,19 +43,21 @@ public class FourDwins{//class name
         }
         }
         int range = Y - X + 1;
-        //generate the length of 3D array
-        int length3D = (int) (range * Math.random()) + X;
-        //System.out.println(length3D);
-        //generate the length of 2D array
-        int length2D = (int) (range * Math.random()) + X;
-        //System.out.println(length2D);
-        //generate the length of 1D array
-        int length1D = (int) (range * Math.random()) + X;
-        //System.out.println(length1D);
-        double[][][][] array = new double[3][length3D][length2D][length1D];
+        double[][][][] array;
+        array = new double[3][][][];
+       // double[][][][] array = new double[3][length3D][length2D][length1D];
         for(int i = 0; i < 3; i++){
+            //generate the length of 3D array
+            int length3D = (int) (range * Math.random()) + X;
+            array[i] = new double[length3D][][];
             for(int j = 0; j < length3D; j++){
+                //generate the length of 2D array
+                int length2D = (int) (range * Math.random()) + X;
+                array[i][j] = new double[length2D][];
                 for(int k = 0; k < length2D; k++){
+                    //generate the length of 1D array
+                    int length1D = (int) (range * Math.random()) + X;
+                    array[i][j][k] = new double[length1D];
                     for(int l = 0; l < length1D; l++){
                         array[i][j][k][l] = ((int)(310 * Math.random())) / 10.0;//1 decimal point
                     }//end of 4th for loop
@@ -74,26 +76,26 @@ public class FourDwins{//class name
         System.out.print("{");
         for(int i = 0; i < 3; i++){
             System.out.print("{");
-            for(int j = 0; j < array[0].length; j++){
+            for(int j = 0; j < array[i].length; j++){
                 System.out.print("{");
-                for(int k = 0; k < array[0][0].length; k++){
+                for(int k = 0; k < array[i][j].length; k++){
                     System.out.print("{");
-                    for(int l = 0; l < array[0][0][0].length; l++){
-                        if(l == array[0][0][0].length - 1){
+                    for(int l = 0; l < array[i][j][k].length; l++){
+                        if(l == array[i][j][k].length - 1){
                             System.out.print(array[i][j][k][l]);
                         }
                         else{
                         System.out.print(array[i][j][k][l] + ", ");
                         }
                     }//end of 4th for loop
-                    if(k == array[0][0][0].length - 1){
+                    if(k == array[i][j][k].length - 1){
                     System.out.print("}");
                 }
                 else {
                     System.out.print("}, ");
                 }
                 }//end of 3rd for loop
-              if(j == array[0][0].length - 1){
+              if(j == array[i][j].length - 1){
                 System.out.print("}");
             }
             else{
@@ -113,13 +115,13 @@ public class FourDwins{//class name
         //System.out.println(array[0].length);
         //System.out.println(array[0][0].length);
         for(int i = 0; i < array.length;i++ ){
-            for(int j = 0;j < array[0].length;j++){
-                for(int k = 0;k < array[0][0].length;k++ ){
+            for(int j = 0;j < array[i].length;j++){
+                for(int k = 0;k < array[i][j].length;k++ ){
                     double min = array[i][j][k];
                     //System.out.println(min);
                     double temp = 0;
                     int index = k;
-                        for(int m = k + 1;m < array[0][0].length;m++)
+                        for(int m = k + 1;m < array[i][j].length;m++)
                         if (min > array[i][j][m]){
                             min = array[i][j][m];
                             index = m;
@@ -139,13 +141,37 @@ public class FourDwins{//class name
         for(int i = 0; i < 3; i++){
             sort3DArray(array[i]);
         }
+        int index1 = 0;
+        int index2 = 0;
+        for(int i = 1; i < 3; i++){
+            for(int j = i - 1;j >= 0; j--){
+                index1 = j + 1;
+                index2 = j;
+                if (array[index1].length < array[index2].length){
+                double[][][] temp;
+                //swap
+                temp = array[index1];
+                array[index1] = array[index2];
+                array[index1] = temp;
+            }
+                else if(array[index1].length == array[index2].length){
+                    if(minArray(array[index1]) < minArray(array[index2])){
+                        double[][][] temp;
+                        //swap
+                        temp = array[index1];
+                        array[index1] = array[index2];
+                        array[index1] = temp;
+                    }
+                }
+            }
+        }
     }
     public static void statArray(double[][][][] array){
         double sum = 0;
         for(int i = 0; i < 3; i++){
-            for(int j = 0; j < array[0].length; j++){
-                for(int k = 0; k < array[0][0].length; k++){
-                    for(int l = 0; l < array[0][0][0].length; l++){
+            for(int j = 0; j < array[i].length; j++){
+                for(int k = 0; k < array[i][j].length; k++){
+                    for(int l = 0; l < array[i][j][k].length; l++){
                         sum += array[i][j][k][l];//sum up the members
                     }//end of 4th for loop
                 }//end of 3rd for loop
@@ -157,4 +183,17 @@ public class FourDwins{//class name
         System.out.println("Sum : " + (int)(sum * 10) / 10.0);
         System.out.println("Mean: " + (int)(mean * 10) / 10.0);
     }
+    public static double minArray(double[][][] array){
+        double min = 0;
+        for(int i = 0; i < array.length;i++){
+            for(int j = 0; j < array[i].length;j++){
+                for(int k = 0;k < array[i][j].length;k++){
+                    if(min > array[i][j][k]){
+                        min = array[i][j][k];
+                    }
+                }
+            }
+        }
+        return min;
+    }//end of minArray method
 }//end of class
